5f1ddf140689eab5e359de7ffcbded4ca795315e
Linus Grebing <linus.frederik.grebing@iem.thm.de>
2022-07-13 21:26:36 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row pt-3 pb-5 align-items-center">
+        <div class="col-3">
+            <img class="w-100 h-auto rounded-circle my-auto" src="https://kartolomio.de/wp-content/uploads/2021/08/GOL-EG-12-1837.jpg" alt="">
+        </div>
+        <div class="col-9 p-5">
+            <div>
+                <h1>BenutzerHeader</h1>
+            </div>
+            <div class="d-flex">
+                <div class="px-2"><strong>155</strong> posts</div>
+                <div class="px-2"><strong>255</strong> followers</div>
+                <div class="px-2"><strong>355</strong> following</div>
+            </div>
+            <div class="py-4">
+                <strong>Meine Profilüberschrift</strong>
+                <p>dieser Space kann genutzt werden um zur Beschreibung des Profils beizutragen</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img class="w-100 h-100" src="https://wallpaperaccess.com/full/2029165.jpg" alt="">
+        </div>
+        <div class="col-4">
+        <img class="w-100 h-100" src="https://wallpaperaccess.com/full/2029165.jpg" alt="">
+        </div>
+        <div class="col-4">
+        <img class="w-100 h-100" src="https://wallpaperaccess.com/full/2029165.jpg" alt="">
+        </div>
+    </div>
+</div>
+@endsection
