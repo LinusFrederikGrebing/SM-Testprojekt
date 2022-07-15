cb325a9203cc0385f32756a23efb16190f61f576
Linus Grebing <linus.frederik.grebing@iem.thm.de>
2022-07-15 17:34:20 +0200

    profile details and stuff like that

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,16 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
+
 {
     //
 
 
-    public function index($user)
+    public function index(\App\Models\User $user)
     {
-      $user = User::findOrFail($user);
-      return view('profiles.index', ['user' => $user] );
+      return view('profiles.index', compact('user') );
+    }
+
+    public function edit(\App\Models\User $user){
+      $this->authorize('update', $user->profile);
+      return view('profiles.edit', compact('user') );
+    }
+
+    public function update(Request $request, \App\Models\User $user){
+      $this->authorize('update', $user->profile);
+   $data = $request->validate([
+      'title' => '',
+      'description' => '',
+      'url' => 'url',
+      'image' => 'image',
+    ]);
+
+    if(request('image')){
+
+      $filepath = request('image')->store('profile', 'public');
+
+      $image = Image::make(public_path("storage/{$filepath}"))->fit(1000, 1000);
+      $image -> save();
+      auth()->user() -> profile->update(array_merge($data, ['image'=> $filepath] ));
+    }else{
+      auth()->user() -> profile->update($data);
+    } 
+
+      
+
+      return redirect("/profile/{$user -> id}");
     }
 
 }
